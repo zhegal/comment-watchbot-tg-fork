129736024e3484c57511e7a812106b91191dfda1
ZAZiOs <85897688+ZAZiOs@users.noreply.github.com>
2025-05-03 22:50:56 +0300

    Whitelist UPDATE

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import 'dotenv/config'
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const ctch = (err) => console.log(err)
 
+const whitelist = [1010473369, 5436194611];
+const start_msg = `<b>МЯУ-МЯУ-МЯУ! НИЗЯ ЭТУ КОМАНДУ ТЫКАТЬ, НИЗЯЯ!</b>\n\n<i>Если вы хотите такого же бота-вахтёра, то исходный код вы можете найти тут:</i>\nhttps://github.com/ZAZiOs/comment-watchbot-tg/`;
+                  // ^^^ HTML acceptable
+
 const isAdmin = async (ctx) => {
     let msg = ctx?.update?.message
     let admins = await ctx.getChatAdministrators()
@@ -20,10 +24,15 @@ const isChannel = (ctx) => {
         || (msg?.reply_to_message && msg?.reply_to_message?.sender_chat?.type == 'channel')
 }
 
-bot.on('message', async (ctx) => {
-    if (isChannel(ctx) || await isAdmin(ctx)) return;
+bot.start(async (ctx) => {
+    if (ctx.message.chat.type != "private") return
+    return ctx.replyWithHTML(start_msg)
+})
 
-    let newmemb = ctx?.update?.message?.new_chat_members    
+bot.on('message', async (ctx) => {
+    if (ctx.message.chat.type == "private" || isChannel(ctx) || await isAdmin(ctx) || whitelist.includes(ctx?.from?.id)) return;
+
+    let newmemb = ctx?.update?.message?.new_chat_members
     let msg = ctx?.update?.message
 
     if (msg?.left_chat_member || !msg?.reply_to_message)
@@ -39,6 +48,5 @@ bot.on('message', async (ctx) => {
 })
 
 bot.launch()
-
 
 // I really tried to make this code look not so shit, but it looks like it's impossible, so...
